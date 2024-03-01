7c68ba12835f88807475002b64d46aa79cef82cf
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-03-01 20:53:59 +0100

    ModifiedM lib/views/auth/side_menu.dart  M pubspec.yaml

--- lib/views/auth/side_menu.dart
+++ lib/views/auth/side_menu.dart
@@ -3,6 +3,7 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_animated_button/flutter_animated_button.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sql_client/utils/shared.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import '../../utils/callbacks.dart';
 
@@ -14,6 +15,9 @@ class SideMenu extends StatefulWidget {
 }
 
 class _SideMenuState extends State<SideMenu> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay = DateTime.now();
   final List<Map<String, dynamic>> _runSQLQueries = List<Map<String, dynamic>>.generate(
     10,
     (int index) => <String, dynamic>{
@@ -87,8 +91,97 @@ class _SideMenuState extends State<SideMenu> {
               onPress: export["callback"],
             ),
             const SizedBox(height: 20),
+            AnimatedButton(
+              //width: 300,
+              height: 80,
+              text: "Run Sql Query with\ndatetime variables",
+              selectedTextColor: darkColor,
+              animatedOn: AnimatedOn.onHover,
+              animationDuration: 500.ms,
+              isReverse: true,
+              selectedBackgroundColor: redColor,
+              backgroundColor: purpleColor,
+              transitionType: TransitionType.TOP_TO_BOTTOM,
+              textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+              onPress: export["callback"],
+            ),
+            const SizedBox(height: 20),
+            StatefulBuilder(
+              key: null,
+              builder: (BuildContext context, void Function(void Function()) _) {
+                return Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: scaffoldColor),
+                        child: TextField(
+                          controller: _from,
+                          readOnly: true,
+                          style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                          decoration: InputDecoration(
+                            hintText: "From",
+                            border: InputBorder.none,
+                            contentPadding: const EdgeInsets.all(8),
+                            hintStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: scaffoldColor),
+                        child: TextField(
+                          controller: _to,
+                          readOnly: true,
+                          style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                          decoration: InputDecoration(
+                            hintText: "To",
+                            border: InputBorder.none,
+                            contentPadding: const EdgeInsets.all(8),
+                            hintStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+            const SizedBox(height: 20),
             Row(
-              children: <Widget>[],
+              children: <Widget>[
+                Expanded(
+                  child: TableCalendar(
+                    firstDay: DateTime(1970),
+                    lastDay: DateTime(2300),
+                    focusedDay: _focusedDay,
+                    calendarFormat: _calendarFormat,
+                    selectedDayPredicate: (DateTime day) => isSameDay(_selectedDay, day),
+                    onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+                      if (!isSameDay(_selectedDay, selectedDay)) {
+                        // Call `setState()` when updating the selected day
+                        setState(() {
+                          _selectedDay = selectedDay;
+                          _focusedDay = focusedDay;
+                        });
+                      }
+                    },
+                    onFormatChanged: (format) {
+                      if (_calendarFormat != format) {
+                        // Call `setState()` when updating calendar format
+                        setState(() {
+                          _calendarFormat = format;
+                        });
+                      }
+                    },
+                    onPageChanged: (focusedDay) {
+                      // No need to call `setState()` here
+                      _focusedDay = focusedDay;
+                    },
+                  ),
+                ),
+              ],
             ),
           ],
         ],
